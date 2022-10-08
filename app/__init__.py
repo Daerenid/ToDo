89bfe05f7e52d23b43a154c658f0a5a702d20a26
Daerenid <patdaw04@gmail.com>
2022-10-08 18:28:13 +0200

    Basic project structure

---
+++ app/__init__.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import os
+from typing import Optional
+
+import eventlet
+from dotenv import load_dotenv
+from eventlet import wsgi
+from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_session import Session
+
+__all__: list[str] = []
+PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
+load_dotenv(PARENT_DIR)
+
+
+def get_flag(name: str) -> bool:
+    return os.environ.get(name) in ("True", "true", "1")
+
+
+def get_env(name: str) -> Optional[str | int]:
+    env = os.environ.get(name)
+    if env is None:
+        return None
+    if env.isdigit():
+        return int(env)
+    return env
+
+
+def setup_session(app: Flask) -> None:
+    """Server side sessions setup."""
+    session_path = os.path.join(app.instance_path, "sessions")
+    app.config["SESSION_FILE_DIR"] = session_path
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SESSION_COOKIE_SECURE"] = True
+    app.config["SESSION_COOKIE_HTTPONLY"] = True
+
+    Session(app)
+
+
+def create_app() -> Flask:
+    """Flask application factory function."""
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+        template_folder="./templates",
+        static_folder="./static",
+    )
+
+    app.config["SECRET_KEY"] = get_env("SECRET_KEY")
+
+    if not os.path.exists(app.instance_path):
+        os.makedirs(app.instance_path)
+
+    setup_session(app)
+
+    @app.cli.command("run-eventlet")
+    def run_eventlet() -> None:
+        """Application startup definition."""
+        address = get_env("ADDRESS")
+        port = get_env("PORT")
+
+        if app.debug:
+            print("[INFO] Starting eventlet in debug mode...")
+            from werkzeug._reloader import run_with_reloader
+
+            def run_server() -> None:
+                eventlet_socket = eventlet.listen((address, port))
+                wsgi.server(eventlet_socket, app, debug=True)
+
+            run_with_reloader(run_server)
+        else:
+            print("[INFO] Starting eventlet...")
+            eventlet_socket = eventlet.listen((address, port))
+            wsgi.server(eventlet_socket, app, debug=False)
+
+    @app.route("/")
+    def home() -> str:
+        return render_template("home.html")
+
+    return app
